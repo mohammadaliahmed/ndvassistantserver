1ff2e959d461a1d12d62cf2de382191d4258d6d7
Ali AHmed <m.aliahmed0@gmail.com>
2021-01-13 09:36:47 +0500

    fcmkchange

--- app/SendNotification.php
+++ app/SendNotification.php
@@ -8,7 +8,7 @@ class SendNotification extends Model
 {
     public function sendPushNotification($fcm_token, $title, $message, $id, $type)
     {
-        $push_notification_key = 'AAAAUqziML4:APA91bHGCbVPdtt__PA-9ljTdtTMquQZer1N3-orYGOYPuQFNt9Bl8yvxJ-yqlVUx71nzjwxsbTBbxhHConkwUkTp5WwZna7E0l3nTli2YpizQb_B7zZ6MN-CMKcMN9gmKKeXKcIP9eF';
+        $push_notification_key = 'AAAA0YUiLng:APA91bFaftYj6m-p6NpIkXpS2xLSFbnnBhYz-J1AHjA1f6gRkK3cpFH5SxPHgnUUJ11kNo9jTyDxmSKpPRTWwOmi3SBXVwaztf08iRQuQV6u51NuqasZ6XnOa4bivL4Ens-6z_OgJN5b';
         $url = "https://fcm.googleapis.com/fcm/send";
         $header = array("authorization: key=" . $push_notification_key . "",
             "content-type: application/json"
